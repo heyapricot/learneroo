ac7208b271baeccc256bb580fb12d7a1171a86a9
Michael Palhidai <mpalhidai@gmail.com>
2018-04-18 08:55:27 -0700

    revised dfs method

--- graphs/dfs.rb
+++ graphs/dfs.rb
@@ -14,7 +14,6 @@ class Node
   end
 end
 
-
 def array2graph(connections)
   connections.length.times do |i|
     Node.new(i)
@@ -23,12 +22,22 @@ def array2graph(connections)
   connections.each_with_index do |conn, idx|
     conn.each do |c|
       Node.graph[idx].connections[c] = Node.graph[c]
-    end  
+    end
   end
+end
+
+def dfs(node)
+#   Print the value of the Node N.
+# Mark N as visited.
+# Get all the adjacent nodes to N.
+# Call DFS on every adjacent Node that's not visited.
+  print "#{node.data} "
+  
+
 end
 
 conns = [[2], [4], [5, 0, 3], [5, 2], [1, 5], [4, 2, 3]]
 
 array2graph(conns)
-
-puts dfs
+node = Node.graph[0]
+dfs(node)
