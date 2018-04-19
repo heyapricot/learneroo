1d726fb2d0e33bdf07c9828387b14a2511644c3a
Michael Palhidai <mpalhidai@gmail.com>
2018-04-19 08:12:41 -0700

    one liners dfs

--- graphs/dfs.rb
+++ graphs/dfs.rb
@@ -16,35 +16,24 @@ class Node
 end
 
 def array2graph(connections)
-  connections.length.times do |i|
-    Node.new(i)
-  end
-
-  connections.each_with_index do |conn, idx|
-    conn.each do |c|
-      Node.graph[idx].connections[c] = Node.graph[c]
-    end
-  end
+  connections.length.times { |i| Node.new(i) }
+  connections.each_with_index { |conn, idx| conn.each { |c| Node.graph[idx].connections[c] = Node.graph[c] } }
 end
 
 def dfs(node)
-#   Print the value of the Node N.
+# Print the value of the Node N.
 # Mark N as visited.
 # Get all the adjacent nodes to N.
 # Call DFS on every adjacent Node that's not visited.
   print "#{node.data} "
   node.visited = true
-  node.connections.each_value do |conn|
-    dfs(conn) unless conn.visited
-  end
+  node.connections.each_value { |conn| dfs(conn) unless conn.visited }
 end
 
-def do_stuff
-  conns = [[2], [4], [5, 0, 3], [5, 2], [1, 5], [4, 2, 3]]
-
+def do_stuff(conns)
   array2graph(conns)
   node = Node.graph[0]
   dfs(node)
 end
 
-puts do_stuff
+do_stuff([[2], [4], [5, 0, 3], [5, 2], [1, 5], [4, 2, 3]])
