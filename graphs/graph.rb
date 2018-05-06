c56499378a0d9811dd19cd26d97a0bd7f7d239c4
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2018-05-06 12:42:52 -0500

    Implemented bfs and testing

--- graphs/graph.rb
+++ graphs/graph.rb
@@ -18,17 +18,38 @@ class Graph
     @nodes = Hash.new
   end
 
-  def addNode(node)
+  def add(node)
     @nodes[node.data] = node
   end
 
-  def getNode(data)
-    @nodes[data]
+  def fromArray(ar)
+    ar.each_with_index do |val,i|
+      add(Node.new(i,val))
+    end
   end
 
-  def self.fromArray(ar)
-    ar.length.times do |i|
-      Node.new(i,ar[i])
+  def bfs(start = 0, queue = [], output = [])
+
+    node = nodes[start]
+
+    unless node.visited
+      output.push(node.data)
+      node.connections.each {|conn| queue.push(conn) unless nodes[conn].visited }
+      node.visited = true
+    end
+
+    bfs(queue.shift(),queue, output) unless queue.empty?
+
+    #End of bfs execution
+    unvisit_nodes
+    output
+  end
+
+  private
+
+  def unvisit_nodes
+    nodes.values.each do |node|
+      node.visited = false
     end
   end
 end
--- spec/graph_spec.rb
+++ spec/graph_spec.rb
@@ -11,6 +11,7 @@ describe Node do
         expect(node.connections).not_to be_nil
       end
     end
+
 end
 
 
@@ -21,22 +22,56 @@ describe Graph do
   it "can add Nodes to itself" do
     NUMBER_OF_NODES = 10
     NUMBER_OF_NODES.times do |n|
-      graph.addNode(Node.new(n))
+      graph.add(Node.new(n))
     end
     expect(graph.nodes.length).to be NUMBER_OF_NODES
   end
 
   it "can get a specific node" do
-
-
+    NODE_DATA = 6
+    graph.add(Node.new(NODE_DATA))
+    expect(graph.nodes[NODE_DATA].data).to be NODE_DATA
   end
+
 
   it "can create a Graph from an array" do
+    ar = [[2], [4], [5, 0, 3], [2], [1, 5], [4, 2]]
+    graph.fromArray(ar)
+
+    ar.each_with_index do |val, i|
+      expect(graph.nodes[i].connections).to be val
+    end
+  end
+
+  it "can get a node from it's connections property" do
+    ar = [[2], [4], [5, 0, 3], [2], [1, 5], [4, 2]]
+    graph.fromArray(ar)
+
+    graph.nodes.each do |data, node|
+      node.connections.each do |conn|
+        expect(conn).to be graph.nodes[conn].data
+      end
+    end
 
   end
 
 
-  it "can traverse nodes using breadth first search" do
+  context "doing breadth first search" do
+    it "returns an array of all node data using breadth first search" do
+      ar = [[2], [4], [5, 0, 3], [2], [1, 5], [4, 2]]
+      graph.fromArray(ar)
+      expect(graph.bfs).to match_array([0,2,5,3,4,1])
+    end
 
+    it "keeps node.visited as false after running" do
+      ar = [[2], [4], [5, 0, 3], [2], [1, 5], [4, 2]]
+      graph.fromArray(ar)
+      graph.bfs
+      graph.nodes.values.each do |node|
+        expect(node.visited).to be false
+      end
+    end
   end
+
+
 end
