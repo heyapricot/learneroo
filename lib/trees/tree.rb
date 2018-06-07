f5438593002e4d9ae4fe833afaff71d5e1d12521
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2018-06-07 10:50:50 -0500

    Improved fromArray method and made it ignore array spaces that contain '0'

--- lib/trees/tree.rb
+++ lib/trees/tree.rb
@@ -21,20 +21,32 @@ module Tree
 
       node.data = array[idx]
 
-      unless (2 * idx) + 1 > (array.length - 1)
-        fromArray(array, (2 * idx) + 1, node.left = Node.new)
-        fromArray(array, (2 * idx) + 2, node.right = Node.new)
-      end
+      l = (2 * idx) + 1
+      r = (2 * idx) + 2
+
+      fromArray(array,l, node.left = Node.new) unless l > (array.length - 1) || array[l] == 0
+      fromArray(array,r, node.right = Node.new) unless r > (array.length - 1) || array[r] == 0
 
     end
 
     def preOrder(node = @head, output = [])
       output.push(node.data)
-      preOrder(node.left,output) unless node.left.nil? || node.left.data == 0
-      preOrder(node.right,output) unless node.right.nil? || node.right.data == 0
+      preOrder(node.left,output) unless node.left.nil?
+      preOrder(node.right,output) unless node.right.nil?
 
       output
     end
+
+
+    def leftCount(node = @head)
+      unless node.left.nil?
+        count = leftCount(node.left)
+        return count + 1
+      else
+        return 0
+      end
+    end
+
 
   end
 end
--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -10,3 +10,18 @@ describe "Tree.preOrder" do
     expect(tree.preOrder).to match_array([3,7,2,5,11,6,7,4,9,5,1])
   end
 end
+
+describe "Tree.isBalanced?" do
+
+  let(:tree){Tree::Tree.new}
+
+  it "returns true if the tree is height-balanced" do
+    tree.fromArray([1,2,3,4,5,6,7])
+    expect(tree.isBalanced?).to be(true)
+  end
+
+  it "returns false if the tree isn't height-balanced" do
+    tree.fromArray([1,2,3,4,0,0,5,6,0,0,0,0,0,0,7])
+    expect(tree.isBalanced?).to be(false)
+  end
+end
